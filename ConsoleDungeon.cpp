e930d720878d887da4e6198db2fe0939dad0afd0
Michael King <michael.ac.king@outlook.com>
2020-06-27 01:43:28 -0400

    Extract declarations to a header.

--- ConsoleDungeon.cpp
+++ ConsoleDungeon.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-#include <conio.h>
-#include <array>
-
-void StartNewGame();
-void DrawBoard();
-void GetKeyPress();
-void CheckPosition();
-
-int size;
-std::string player;
-std::string key;
-std::string door;
-std::array<int, 2> playerPos { 1, 1 };
-std::array<int, 2> keyPos { 4, 6 };
-std::array<int, 2> doorPos { 0, 0 };
-bool isGameOver;
-bool hasKey;
+#include <ConsoleDungeon.h>
 
 int main()
 {
